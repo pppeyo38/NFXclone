8498284f30143d265e9d1efb9c2d348831a1d97b
pppeyo38 <fuwafuwa.melon47@gmail.com>
2022-04-04 13:41:09 +0900

    Display Trailer

--- nfx_clone/src/components/Row.tsx
+++ nfx_clone/src/components/Row.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import YouTube from "react-youtube";
+import YouTube from "react-youtube";
 import axios from '../getDB/axios';
 // stylesheets
 import "../stylesheets/Row.scss";
@@ -22,8 +22,18 @@ type Movie = {
 	backdrop_path: string;
 };
 
+// trailerのoption
+type Options = {
+	height: string;
+	width: string;
+	playerVars: {
+		autoplay: 0 | 1 | undefined;
+	};
+};
+
 export const Row = ({  title, fetchUrl, isLargeRow }: Props) => {
 	const [movies, setMovies] = useState<Movie[]>([]);
+	const [trailerUrl, setTrailerUrl] = useState<string | null>("");
 
 	// 非同期処理？
 	// ページの表示をいち早く行って、非同期処理を使って後から処理が終わった順でAPIデータを入れていく
@@ -38,6 +48,27 @@ export const Row = ({  title, fetchUrl, isLargeRow }: Props) => {
 		fetchData();
 	}, [fetchUrl]);
 
+	const opts: Options = {
+		height: "390",
+		width: "640",
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1,
+		},
+	};
+
+	// トレイラーにyoutubeの映像を呼び出す
+	const handleClick = async (movie: Movie) => {
+		if (trailerUrl) {
+			setTrailerUrl("");
+		} else {
+			let trailerurl = await axios.get(
+				`/movie/${movie.id}/videos?api_key=~~~`
+			);
+			setTrailerUrl(trailerurl.data.results[0]?.key);
+		}
+	}
+
 	return (
 		<div className="Row">
 			<h2>{ title }</h2>
@@ -51,11 +82,11 @@ export const Row = ({  title, fetchUrl, isLargeRow }: Props) => {
 							isLargeRow ? movie.poster_path : movie.backdrop_path
 						}`}
 						alt={movie.name}
-						// onClick={() => handleClick(movie)}
+						onClick={() => handleClick(movie)}
 					/>
 				))}
 			</div>
-			{/* {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />} */}
+			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
 		</div>
 	);
 };
